1779e7b6420280d89dd34a27c2a4629d309927b3
kserniukCS3035 <kristyn.serniuk@unb.ca>
2023-11-25 14:43:25 -0400

    Partially working deliverable dialog- still dosnt add to the database tho

---
+++ app/src/main/java/ca/unb/mobiledev/group18project/ui/deliverables/DeliverableAdapter.kt
@@ -0,0 +1,66 @@
+package ca.unb.mobiledev.group18project.ui.deliverables
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import ca.unb.mobiledev.group18project.R
+import ca.unb.mobiledev.group18project.entities.Course
+import ca.unb.mobiledev.group18project.entities.Deliverable
+import ca.unb.mobiledev.group18project.ui.singlecourse.SingleCourseFragment
+
+class DeliverableAdapter(context: Context, items: List<Deliverable>, private val viewmodel: DeliverablesViewModel, private val fragment: DeliverablesFragment) : ArrayAdapter<Deliverable>(
+    context, 0, items) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        // Get the data item for this position
+        val item = getItem(position)
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        var currView = convertView
+        if (currView == null) {
+            currView = LayoutInflater.from(context).inflate(R.layout.deliverablelist_layout, parent, false)
+        }
+
+        // Lookup view for data population
+        val deliverableCourse= currView!!.findViewById<TextView>(R.id.deliverable_course)
+        val deliverableName = currView!!.findViewById<TextView>(R.id.deliverable_name)
+        val deliverableDate = currView!!.findViewById<TextView>(R.id.deliverable_date)
+        val deliverableWorth = currView!!.findViewById<TextView>(R.id.deliverable_worth)
+
+
+        val DeliverableMenu = currView!!.findViewById<ImageView>(R.id.image_menu)
+
+        deliverableName.text = item!!.name
+        //deliverableDate.text = item.weight
+
+        /**
+        deliverableMenu.setOnClickListener {
+            val popup = PopupMenu(context, deliverableMenu)
+            popup.inflate(R.menu.courselist_menu)
+            popup.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.menu_option_edit -> {
+                        fragment.BuildDialog("EDIT DELIVERABLE", item, false)
+                        true
+                    }
+                    R.id.menu_option_delete -> {
+                        viewmodel.delete(item)
+                        true
+                    }
+                    else -> false
+                }
+            }
+            popup.show()
+        }
+        */
+
+        // Return the completed view to render on screen
+        return currView
+    }
+}
--- app/src/main/java/ca/unb/mobiledev/group18project/ui/deliverables/DeliverablesFragment.kt
+++ app/src/main/java/ca/unb/mobiledev/group18project/ui/deliverables/DeliverablesFragment.kt
@@ -1,20 +1,33 @@
 package ca.unb.mobiledev.group18project.ui.deliverables
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import ca.unb.mobiledev.group18project.R
+import ca.unb.mobiledev.group18project.databinding.FragmentCoursesBinding
 import ca.unb.mobiledev.group18project.databinding.FragmentDeliverablesBinding
+import ca.unb.mobiledev.group18project.entities.Course
+import ca.unb.mobiledev.group18project.entities.Deliverable
+import ca.unb.mobiledev.group18project.ui.courses.CoursesViewModel
 
-class DeliverablesFragment : Fragment() {
+class DeliverablesFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentDeliverablesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
+
+    private lateinit var mDeliverablesViewModel: DeliverablesViewModel
+    private lateinit var mListView: ListView
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,22 +35,72 @@ class DeliverablesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val deliverablesViewModel =
-            ViewModelProvider(this).get(DeliverablesViewModel::class.java)
+        mDeliverablesViewModel = ViewModelProvider(this).get(DeliverablesViewModel::class.java)
 
         _binding = FragmentDeliverablesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        /*
-        val textView: TextView = binding.textDeliverables
-        settingsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }*/
+        mListView = root.findViewById(R.id.deliverables_view)
+        val addButton = root.findViewById<Button>(R.id.add_deliverables)
+
+        addButton.setOnClickListener(this)
+
+        mDeliverablesViewModel.allCourses.observe(viewLifecycleOwner) {
+
+        }
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.add_deliverables -> {
+                BuildDialog("ADD DELIVERABLE", null, true)
+            }
+        }
+    }
+
+
+    fun BuildDialog(title: String, deliverable: Deliverable?, new: Boolean) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        val inflater = requireActivity().layoutInflater
+        val dialogView = inflater.inflate(R.layout.dialog_add_deliverable, null)
+        val editTextDeliverableCourse = dialogView.findViewById<EditText>(R.id.editTextCourseName)
+        val editDeliverableName = dialogView.findViewById<EditText>(R.id.editTextDeliverableName)
+        val editDeliverableDate  = dialogView.findViewById<EditText>(R.id.editTextDeliverableDate)
+        val editDeliverableWorth = dialogView.findViewById<EditText>(R.id.editTextDeliverableWorth)
+
+        builder.setView(dialogView)
+            .setTitle(title)
+            .setPositiveButton("Submit") { _, _ ->
+                val cName = editTextDeliverableCourse.text.toString()
+                val dName = editDeliverableName.text.toString()
+                val dDate = editDeliverableDate.text.toString()
+                val dWorth = editDeliverableWorth.text.toString()
+
+
+                if (cName == "" || dName.toIntOrNull() == null) {
+                    Toast.makeText(binding.root.context, "Data entered is incomplete/incorrect format. Data has not been saved.", Toast.LENGTH_SHORT).show()
+                    return@setPositiveButton
+                }
+
+                if (new) {
+                    Toast.makeText(binding.root.context, "New Data Entry", Toast.LENGTH_SHORT).show()
+                    mDeliverablesViewModel.insert(dName,cName.toInt())
+                } else {
+                    Toast.makeText(binding.root.context, "Updated Data Entry", Toast.LENGTH_SHORT).show()
+                    deliverable?.name = dName
+                    deliverable?.courseID  = cName.toInt()
+                    mDeliverablesViewModel.update(deliverable!!)
+                }
+            }
+            .setNegativeButton("Cancel", null)
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
